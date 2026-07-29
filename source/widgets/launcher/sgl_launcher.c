5e4e8bfcc7cdcdaad7cab1c08dd72656eaa218e1
lishanwen <1477153217@qq.com>
2026-07-29 22:22:56 +0800

    chore: optimize launcher

--- source/widgets/launcher/sgl_launcher.c
+++ source/widgets/launcher/sgl_launcher.c
@@ -201,12 +201,7 @@ void sgl_launcher_add_app(sgl_obj_t *launcher, sgl_launcher_app_t *app)
 {
     sgl_launcher_t *launcher_obj = (sgl_launcher_t *)launcher;
     int16_t width = 0;
-
-    sgl_obj_t *icon = sgl_rect_create(launcher);
-    if (icon == NULL) {
-        SGL_LOG_ERROR("sgl_launcher_add_app: sprite create failed");
-        return;
-    }
+    sgl_obj_t *icon = NULL;
 
     sgl_obj_t *label = sgl_label_create(launcher);
     if (label == NULL) {
@@ -229,10 +224,26 @@ void sgl_launcher_add_app(sgl_obj_t *launcher, sgl_launcher_app_t *app)
     int16_t x = launcher_item_pos(launcher_obj->margin_left, launcher_w, cols, launcher_obj->icon_size, col);
     int16_t y = launcher_item_pos(launcher_obj->margin_top,  launcher_h, rows, launcher_obj->icon_size, row);
 
+    if (app->icon->format == SGL_PIXMAP_FMT_RLE_ARGB4444) {
+        icon = sgl_sprite_create(launcher);
+        if (icon == NULL) {
+            SGL_LOG_ERROR("sgl_launcher_add_app: sprite create failed");
+            return;
+        }
+        sgl_sprite_set_pixmap(icon, app->icon);
+    }
+    else if (app->icon->format == SGL_PIXMAP_FMT_RLE_RGB565){
+        icon = sgl_rect_create(launcher);
+        if (icon == NULL) {
+            SGL_LOG_ERROR("sgl_launcher_add_app: sprite create failed");
+            return;
+        }
+        sgl_rect_set_pixmap(icon, app->icon);
+        sgl_rect_set_border_width(icon, 0);
+    }
+
     sgl_obj_set_pos(icon, x + x_ofs, y);
     sgl_obj_set_size(icon, launcher_obj->icon_size, launcher_obj->icon_size);
-    sgl_rect_set_pixmap(icon, app->icon);
-    sgl_rect_set_border_width(icon, 0);
     sgl_obj_set_clickable(icon);
     sgl_obj_set_event_cb(icon, app->event_cb, app->private_data);
 
